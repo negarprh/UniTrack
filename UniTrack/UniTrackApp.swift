09e5ea70fecb2f7ac265a6493d7958a59ea752f8
Negar <negarpr@hotmail.com>
2025-11-03 19:54:37 -0500

    setup project folder and github

---
+++ UniTrack/UniTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniTrackApp.swift
+//  UniTrack
+//
+//  Created by Negar Pirasteh on 2025-11-03.
+//
+
+import SwiftUI
+
+@main
+struct UniTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
